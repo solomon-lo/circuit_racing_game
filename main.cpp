5fdb79b33794a0622712ef6f5db2bbd3a26021d2
Solomon Lo <losolomon0@gmail.com>
2020-12-15 10:23:54 +0800

    instantiate sprites from files

---
+++ main.cpp
@@ -0,0 +1,16 @@
+int main()
+{
+    RenderWindow app(VideoMode(640, 480), "Car Racing Game!");
+    app.setFramerateLimit(60);
+
+    Texture t1,t2;
+    t1.loadFromFile("images/background.png");
+    t2.loadFromFile("images/car.png");
+    t1.setSmooth(true);
+    t2.setSmooth(true);
+
+    Sprite sBackground(t1), sCar(t2);
+    sBackground.scale(2,2);
+
+    return 0;
+}
