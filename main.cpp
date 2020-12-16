b545649044532bcea37a83e6d29a62c2b4abfa8e
Solomon Lo <losolomon0@gmail.com>
2020-12-16 20:59:38 +0800

    start window with car

--- main.cpp
+++ main.cpp
@@ -3,14 +3,38 @@ int main()
     RenderWindow app(VideoMode(640, 480), "Car Racing Game!");
     app.setFramerateLimit(60);
 
-    Texture t1,t2;
+    Texture t1, t2;
     t1.loadFromFile("images/background.png");
     t2.loadFromFile("images/car.png");
     t1.setSmooth(true);
     t2.setSmooth(true);
 
     Sprite sBackground(t1), sCar(t2);
-    sBackground.scale(2,2);
+    sBackground.scale(2, 2);
+
+    const int N = 5;
+    Car car[N];
+    for (int i = 0; i < N; i++)
+    {
+        car[i].x = 300 + i * 50;
+        car[i].y = 1700 + i * 80;
+        car[i].speed = 7 + i;
+    }
+
+    float speed = 0, angle = 0;
+    float maxSpeed = 12.0;
+    float acc = 0.2, dec = 0.3;
+    float turnSpeed = 0.08;
+
+    while (app.isOpen())
+    {
+        Event e;
+        while (app.pollEvent(e))
+        {
+            if (e.type == Event::Closed)
+                app.close();
+        }
+    }
 
     return 0;
 }
