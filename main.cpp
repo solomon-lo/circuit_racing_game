7a6359db062208380fec18bbedc399efbd8f0c01
Solomon Lo <losolomon0@gmail.com>
2021-01-03 21:54:47 +0800

    finalized points system integration

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <SFML/Graphics.hpp>
 using namespace sf;
 
+const int num=8; //checkpoints
+int points[num][2] = {300, 610,
+                      1270,430,
+                      1380,2380,
+                      1900,2460,
+                      1970,1700,
+                      2550,1680,
+                      2560,3150,
+                      500, 3300};
+
+
 struct Car
 {
   float x,y,speed,angle; int n;
